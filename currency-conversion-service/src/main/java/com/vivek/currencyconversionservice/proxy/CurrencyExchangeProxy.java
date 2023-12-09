5bcf181715f0c58f1b35e171f9e7e0da6717e98e
viveksoni100 <viveksoni100@gmail.com>
2023-12-09 14:05:37 +0530

    client side load balancing is learnt usig eureka netflix

--- currency-conversion-service/src/main/java/com/vivek/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/vivek/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,9 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.math.BigDecimal;
-
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000") if you're using eureka server then no need to specify URL
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
